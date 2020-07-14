232f28d2c7870dc31ff6bad0dfeef0b994200b50
wf <wf@zmjx.com>
2020-07-14 15:37:33 +0800

    配置文件修改

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import Test from "./libs/Test";
 
-AppRegistry.registerComponent(appName, () => App);
+export {
+    Test,
+}
